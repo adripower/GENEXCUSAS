68324f496f757293eb503c7b59669187954997bc
adripower <adriantorres.gc@gmail.com>
2023-12-14 02:10:17 +0000

    modified:   .learn/index.html 	modified:   .learn/main.js 	modified:   .learn/styles.css

--- .learn/main.js
+++ .learn/main.js
@@ -1,14 +1,21 @@
+window.onload = function() {
+    //document.querySelector("#boton").addEventListener("click",()=>{       si pongo esta linea el boton me sale los numero 
+    document.querySelector("#excusa").innerHTML = indice();
+    document.querySelector("#excusa").innerHTML = elemento1 + elemento2 + elemento3 + elemento4;
+  };
 
 
 
+// a partir de aqui no se toca nada :P
 function indice(){
     // Generar un índice aleatorio entre 0 y 3
    return Math.floor(Math.random() * 4);
+  
 }
 
 let quien =["El perro ","El gato ","Un ninja ","Un alien "];
 let que =["se lo comio ","lo escondio ","lo rompio ","se le olvido "];
-let donde =["en casa ","en el trabajo ","en la calle ","no se acuerda"];
+let donde =["en casa ","en el trabajo ","en la calle ","no se acuerda "];
 let cuando =["mientras jugaba ","mientras dormia ","cuando venia ","cuando cantaba "];
 
 
